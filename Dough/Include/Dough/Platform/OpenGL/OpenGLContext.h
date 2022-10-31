0cb3688e0d217e015ab7f96066775a4c7c27818a
Matt Ware <mattbware3@gmail.com>
2022-10-31 13:50:14 +0000

    Added OpenGL error callback

--- Dough/Include/Dough/Platform/OpenGL/OpenGLContext.h
+++ Dough/Include/Dough/Platform/OpenGL/OpenGLContext.h
@@ -1,4 +1,6 @@
 #pragma once
+
+#include "glad/gl.h"
 #include "Dough/Renderer/GraphicsContext.h"
 
 struct GLFWwindow;
@@ -12,6 +14,14 @@ namespace Dough
 
 		void Init() override;
 		void SwapBuffers() override;
+
+		static void GLErrorCallback(GLenum source,
+			GLenum type,
+			GLuint id,
+			GLenum severity,
+			GLsizei length,
+			const GLchar* message,
+			const void* userParam);
 	private:
 		GLFWwindow* m_WindowHandle;
 	};
--- Dough/Source/Dough/Platform/OpenGL/OpenGLContext.cpp
+++ Dough/Source/Dough/Platform/OpenGL/OpenGLContext.cpp
@@ -22,10 +22,117 @@ namespace Dough
 		DH_ASSERT_FATAL(version, "Failed to initialise OpenGL via GLAD!");
 		DH_ENGINE_INFO("Loaded OpenGL v{0}", glGetString(GL_VERSION));
 		DH_ENGINE_INFO("Rendering using {0}, drivers from {1}.", glGetString(GL_RENDERER), glGetString(GL_VENDOR));
+
+		// Initialise OpenGL error callback, if not dist build
+#ifndef DH_DIST
+        glEnable(GL_DEBUG_OUTPUT);
+		glDebugMessageCallback(OpenGLContext::GLErrorCallback, nullptr);
+#endif
 	}
 
 	void OpenGLContext::SwapBuffers()
 	{
 		glfwSwapBuffers(m_WindowHandle);
+	}
+
+	void OpenGLContext::GLErrorCallback(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar* message, const void* userParam)
+	{
+#ifndef DH_SHOW_GL_NOTIFICATIONS
+        if (severity == GL_DEBUG_SEVERITY_NOTIFICATION)
+            return;
+#endif
+
+        char* sourceText;
+        char* typeText;
+        char* severityText;
+
+        switch (source) {
+        case GL_DEBUG_SOURCE_API:
+            sourceText = "API";
+            break;
+
+        case GL_DEBUG_SOURCE_WINDOW_SYSTEM:
+            sourceText = "Window System";
+            break;
+
+        case GL_DEBUG_SOURCE_SHADER_COMPILER:
+            sourceText = "Shader Compiler";
+            break;
+
+        case GL_DEBUG_SOURCE_THIRD_PARTY:
+            sourceText = "Third Party";
+            break;
+
+        case GL_DEBUG_SOURCE_APPLICATION:
+            sourceText = "Application";
+            break;
+
+        case GL_DEBUG_SOURCE_OTHER:
+            sourceText = "Unknown";
+            break;
+
+        default:
+            sourceText = "Unknown";
+            break;
+        }
+
+        switch (type) {
+        case GL_DEBUG_TYPE_ERROR:
+            typeText = "Error";
+            break;
+
+        case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR:
+            typeText = "Deprecated Behaviour";
+            break;
+
+        case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:
+            typeText = "Undefined Behaviour";
+            break;
+
+        case GL_DEBUG_TYPE_PORTABILITY:
+            typeText = "Portability";
+            break;
+
+        case GL_DEBUG_TYPE_PERFORMANCE:
+            typeText = "Performance";
+            break;
+
+        case GL_DEBUG_TYPE_OTHER:
+            typeText = "Other";
+            break;
+
+        case GL_DEBUG_TYPE_MARKER:
+            typeText = "Marker";
+            break;
+
+        default:
+            typeText = "Unknown";
+            break;
+        }
+
+        switch (severity) {
+        case GL_DEBUG_SEVERITY_HIGH:
+            severityText = "High";
+            break;
+
+        case GL_DEBUG_SEVERITY_MEDIUM:
+            severityText = "Medium";
+            break;
+
+        case GL_DEBUG_SEVERITY_LOW:
+            severityText = "Low";
+            break;
+
+        case GL_DEBUG_SEVERITY_NOTIFICATION:
+            severityText = "Notification";
+            break;
+
+        default:
+            severityText = "Unknown";
+            break;
+        }
+
+        DH_ENGINE_ERROR("OpenGL Error ({0}, id: {4}): from {1}, {2}: {3}", severityText, sourceText, typeText, message, id);
+        DH_ASSERT_ERROR(severity == GL_DEBUG_SEVERITY_NOTIFICATION);
 	}
 }
